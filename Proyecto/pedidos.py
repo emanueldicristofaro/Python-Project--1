1cca660a1fccdf18507d82ce162857620573e42f
Emanuel Di Cristofaro <emanuelesposito3@gmail.com>
2020-11-18 14:53:14 -0400

    Modificación #2
    -Agregue una parte del pedido de sandwiches, pueden guiarse o mejorarlo..
    -Agregue la parte de pago (Aunque falta una condición)

--- Proyecto/pedidos.py
+++ Proyecto/pedidos.py
@@ -1,3 +1,4 @@
+
 # Toda la lógica principal de los pedidos de sandwiches y bebidas.
 
 def pedidoSandwiches(nombre):
@@ -7,11 +8,16 @@ def pedidoSandwiches(nombre):
     precios_tamano = {'Triple': 580, 'Doble': 430, 'Individual': 280}
     precios_ingredientes = {'Jamon': 40, 'Champiñiones': 35, 'Pimentón': 30, 'Doble Queso': 40, 'Aceitunas': 57.5
     , 'Pepperoni': 38.5, 'Salchichón': 62.5}
+    lista_ingredientes_escogidos = []
+    lista_bebidas_escogidas = []
+    eleccion_escogida = ''
+    lista_bebidas = ['Pepsi', 'Coca Cola', 'Frescolita', 'Chinotto']
+    lista_ingredientes = ['Jamón', 'Champiñiones', 'Pimentón', 'Doble Queso', 'Aceitunas', 'Pepperoni', 'Salchichón']
 
     precios_bebidas = {'Pepsi': 15, 'Coca Cola': 17, 'Frescolita': 14, 'Chinotto': 15}
 
     option = ''
-    while option != '3':
+    while option != '4':
         if option == '1':
             
             print()
@@ -22,7 +28,79 @@ def pedidoSandwiches(nombre):
             eleccion = eleccion.lower()
 
             if eleccion == 't':
-                precio_total = precio_total + precios_tamano.get('Triple')          
+                precio_total = precio_total + precios_tamano.get('Triple')
+                eleccion_escogida = 'Triple'
+
+                print('Haz seleccionado el tamaño triple')
+
+                si_no = 'si'
+                while si_no == 'si':
+                    print()
+                    print('Por favor indique los ingredientes que desee usar')
+                    print('Si no desea ninguno solamente presione (q)')
+                    print()
+                    print('Ingredientes: ')
+                    print('Jamón indique (ja)')
+                    print('Champiñiones indique (ch)')
+                    print('Pimentón indique (pi)')
+                    print('Doble Queso indique (dq)')
+                    print('Aceitunas indique (ac)')
+                    print('Pepperoni indique (pp)')
+                    print('Salchichón indique (sa)')
+
+                    eleccion_s = input('Indique su elección: ')
+                    eleccion_s = eleccion_s.lower()
+
+                    if eleccion_s == 'ja':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[0])
+                        precio_total = precio_total + precios_ingredientes.get('Jamon')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'ch':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[1])
+                        precio_total = precio_total + precios_ingredientes.get('Champiñiones')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'pi':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[2])
+                        precio_total = precio_total + precios_ingredientes.get('Pimentón')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'dq':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[3])
+                        precio_total = precio_total + precios_ingredientes.get('Doble Queso')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'ac':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[4])
+                        precio_total = precio_total + precios_ingredientes.get('Aceitunas')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'pp':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[5])
+                        precio_total = precio_total + precios_ingredientes.get('Pepperoni')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'sa':
+                        lista_ingredientes_escogidos.append(lista_ingredientes[6])
+                        precio_total = precio_total + precios_ingredientes.get('Salchichón')
+
+                        si_no = input('¿Desea agregar otro ingrediente si/no: ')
+
+                    elif eleccion_s == 'q':
+                        print()
+                        print('Sandwich triple con queso')
+                        break
+                        
+                    else:
+                        print('Por favor indique el ingrediente como se le solicita')
+
             elif eleccion == 'd':
                 precio_total = precio_total + precios_tamano.get('Doble') 
             elif eleccion == 'i':
@@ -33,11 +111,38 @@ def pedidoSandwiches(nombre):
         if option == '2':
             print()
 
+        if option == '3':
+
+            print()
+            print(f'Usted seleccionó el sandwich de tamaño {eleccion_escogida}')
+            print(f'Los ingredientes escogidos fueron {lista_ingredientes_escogidos}')
+            print(f'Precio total a pagar {precio_total}')
+
+            si_no_p = input('¿Desea continuar? si/no: ')
+            si_no_p = si_no_p.lower()
+
+            if si_no_p == 'si':
+                print()
+                print(f'EL pedido tiene un total de un sandwich por un monto de {precio_total}')
+                print('Gracias por su compra, regrese pronto')
+                contador += 1
+                eleccion_escogida = ''
+                precio_total = 0
+                lista_ingredientes_escogidos.clear()
+
+            elif si_no_p == 'no':
+                eleccion_escogida = ''
+                precio_total = 0
+                lista_ingredientes_escogidos.clear()
+            
+            else:
+                print('Por favor indique si/no')
+
         print()
         print(f'Bienvenido a sandwiches GUNS N ROSES {nombre}')
         print()
         print(f'Sandwich número {contador}')
-        print(f'Precio total del pedido {precio_total}')
+        print(f'Precio total del pedido {precio_total} BsS')
         print()
 
         print('Sandwiches...')
@@ -48,6 +153,6 @@ def pedidoSandwiches(nombre):
         print('"Los mejores sandwiches que podrás comer"')
 
         print()
-        option = input('\nMenú de Alimentos\n(1) Sandwich\n(2) Bebidas\n(3) Terminar\nElige una opción:')
+        option = input('\nMenú de Alimentos\n(1) Sandwich\n(2) Bebidas\n(3) Pagar\n(4) Salir\nElige una opción:')
 
 # ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
